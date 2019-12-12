4ac3a84c85991194791838a9a8ad41b42a538da3
DBCDaniel <danielpaulse@gmail.com>
2019-12-12 17:22:50 +0100

    - Changes to node.py   - Added create_keys request placeholder   - Added load_keys request placeholder - Changes to node.html   - finished error label functionality   - finished success label functionality   - finished on create wallet functionality   - finished on load wallet functionality   - finished wallet content switching functionality

--- SimpleBlockchain/SimpleBlockchain/node.py
+++ SimpleBlockchain/SimpleBlockchain/node.py
@@ -15,6 +15,24 @@ def get_network_ui():
     return send_from_directory("ui", "network.html")
 #WEB INTERFACE END
 
+#WALLET REQUESTS START
+@app.route("/wallet", methods=["POST"])
+def create_keys():
+    response = {
+        "message": "requested functionality is currently not implemented"
+    }
+    return jsonify(response), 501
+
+
+@app.route("/wallet", methods=["GET"])
+def load_keys():
+    response = {
+        "message": "requested functionality is currently not implemented"
+    }
+    return jsonify(response), 501
+#WALLET REQUESTS END
+
+
 if __name__ == "__main__":
     blockchain = Blockchain()
     app.run(host="0.0.0.0", port=5000)
