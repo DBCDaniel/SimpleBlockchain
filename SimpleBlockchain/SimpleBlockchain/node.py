533ee3ca965ef0a180d88d3c59159c4d72f3bb90
DBCDaniel <danielpaulse@gmail.com>
2019-12-12 09:13:13 +0100

    - Changed node.py to startup file - Changes to node.py   - imported flask & flask_cors   - created get node ui function   - created localhosted flask server running on port 5000

--- SimpleBlockchain/SimpleBlockchain/node.py
+++ SimpleBlockchain/SimpleBlockchain/node.py
@@ -1,1 +1,14 @@
+from flask import Flask, jsonify, request, send_from_directory;
+from flask_cors import CORS
 from blockchain import Blockchain
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/", methods=["GET"])
+def get_node_ui():
+    return send_from_directory("ui", "node.html")
+
+if __name__ == "__main__":
+    blockchain = Blockchain()
+    app.run(host="0.0.0.0", port=5000)
