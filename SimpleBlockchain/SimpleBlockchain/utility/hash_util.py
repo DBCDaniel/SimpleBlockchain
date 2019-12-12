51a75defc8f52b1a73c7b2b76ce498ccf22f7762
DBCDaniel <danielpaulse@gmail.com>
2019-12-12 07:54:40 +0100

    - Added hash_util.py to utility package   - imported hashlib & json   - created hash string 256 function   - created hash block function

---
+++ SimpleBlockchain/SimpleBlockchain/utility/hash_util.py
@@ -0,0 +1,21 @@
+import hashlib as hl
+import json
+
+def hash_string_256(string):
+    """Creates a SHA256 hash for a given input string.
+
+    Arguments:
+        :string: The string which should be hashed.
+    """
+    return hl.sha256(string).hexdigest()
+
+def hash_block(block):
+    """Hashes a block and returns a string representation of it.
+
+    Arguments:
+        :block: The block that should be hashed.
+    """
+    hashable_block = block.__dict__.copy()
+    json_block = json.dumps(hashable_block, sort_keys=True)
+    encoded_json_block = json_block.encode()
+    return hash_string_256(encoded_json_block)
